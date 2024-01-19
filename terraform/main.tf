2cad00f9b6cff3639efb2fd4ffc6ec84799b5614
BNSBNS <y3kmarvin@gmail.com>
2024-01-20 02:15:16 +0800

    test actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,31 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+
+/*
+terraform {
+  required_providers {
     docker = {
       source  = "kreuzwerker/docker"
       version = "~> 3.0.1"
@@ -14,6 +40,7 @@ resource "docker_image" "nginx" {
   keep_locally = false
 }
 
+# resource type, name
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
   name  = "tutorial"
@@ -23,3 +50,4 @@ resource "docker_container" "nginx" {
     external = 8000
   }
 }
+*/
